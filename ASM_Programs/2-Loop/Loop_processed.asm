7dca891f98ee9fa879e51fc2c38dff57a34e7a55
Leonardo <leonardo.cattarin@gmail.com>
2022-06-20 21:09:23 +0200

    Updated Processed Asm for bug fix

--- ASM_Programs/2-Loop/Loop_processed.asm
+++ ASM_Programs/2-Loop/Loop_processed.asm
@@ -1,5 +1,5 @@
 //jump directly to instructions
-{00 01}JMP PTR[LOOP] (2) [C3 16]
+{00 01}JMP PTR[LOOP] (2) [C3 15]
 
 //memory location of counter value and initial address for number writing
 {02}COUNTER = 0 (1) [00]
@@ -29,29 +29,31 @@
 {11}CMP B (1) [B8]
 //check if zero flag is 1 (A=B) and jump to end program if true
 {12}CHZ (1) [CC]
-{13 14}JC PTR[AFTER_LOOP] (2) [DA 24]
+{13 14}JC PTR[AFTER_LOOP] (2) [DA 23]
 
 #LOOP# {15}
-//puts counter value in A
+//puts counter value in A and C
 {15 16}MVI B,PTR[COUNTER] (2) [06 02]
 {17}MOV H,B (1) [60]
 {18}MOV B, M[H] (1) [46]
 {19}MOV A,B (1) [78]
+{1A}MOV C,B (1) [48]
 
 //calculates address to write counter value, put in H
-{1A 1B}MVI B,PTR[NUMBERS_ADDR] (2) [06 04]
-{1C}ADD B (1) [80]
-{1D}MOV B,A (1) [47]
-{1E}MOV H,B (1) [60]
+{1B 1C}MVI B,PTR[NUMBERS_ADDR] (2) [06 04]
+{1D}ADD B (1) [80]
+{1E}MOV B,A (1) [47]
+{1F}MOV H,B (1) [60]
 
-//re-load counter value in B and write it to memory
-{1F}MOV B,A (1) [47]
-{20}MOV M[H],B (1) [70]
+//re-load counter value in B and A from C and write it to memory
+{20}MOV B,C (1) [47]
+{21}MOV A,B
+{22}MOV M[H],B (1) [70]
 
 //jump to increment routine
-{21 22}JMP [INCREMENT] (2) [C3 05]
+{23 24}JMP [INCREMENT] (2) [C3 05]
 
-#AFTER_LOOP# {23}
-{23}HLT (1) [76]
+#AFTER_LOOP# {25}
+{25}HLT (1) [76]
 
 #NUMBERS_ADDR#{27}
